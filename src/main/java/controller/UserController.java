5af83e227ec862da689f618da9f9331341d1e4d0
NoOneBetterThanMrMasters <verdant6th@gmail.com>
2022-07-06 12:25:19 +0300

    edited

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -20,7 +20,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public String getIndex(Model model){
         model.addAttribute("users", userService.getListUsers());
         return "index";
@@ -40,7 +40,7 @@ public class UserController {
     @PostMapping
     public String create(@ModelAttribute("user") User user) {
         userService.add(user);
-        return "redirect:/users";
+        return "redirect:/";
     }
 
     @GetMapping("/{id}/edit")
@@ -52,12 +52,12 @@ public class UserController {
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") User user, @PathVariable("id") int id ) {
         userService.update(user, id);
-        return "redirect:/users";
+        return "redirect:/";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         userService.delete(id);
-        return "redirect:/users";
+        return "redirect:/";
     }
 }
